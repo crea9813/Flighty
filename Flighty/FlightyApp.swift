0ce4ff0ecb613289fbd0e826e90b2fba6660bd05
Yang <38071744+crea9813@users.noreply.github.com>
2022-11-17 17:39:32 +0900

    Initial Commit

---
+++ Flighty/FlightyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightyApp.swift
+//  Flighty
+//
+//  Created by SuperMove on 2022/11/17.
+//
+
+import SwiftUI
+
+@main
+struct FlightyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
